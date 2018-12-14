9c06921fdb338af18c5d8b027c1997823c0afbe2
bakenray <lpqstrong@163.com>
2018-12-14 17:52:39 +0800

    admin

--- src/js/songList.js
+++ src/js/songList.js
@@ -6,9 +6,8 @@
             let $el =$(this.el)
             $el.html(this.template)
             let {songs} = data
-            let liList =songs.map((song)=>{$('<li></li>').text(song.name)})
+            let liList =songs.map((song)=>$('<li></li>').text(song.name))
             $el.find('ul').empty()
-
             liList.map((domLi)=>{
                 $el.find('ul').append(domLi)
             })
@@ -31,9 +30,7 @@
                 this.view.clearActive()
             })
             window.eventHub.on('created',(songData)=>{
-
                 this.model.data.songs.push(songData)
-                console.log(this.model.data)
                 this.view.render(this.model.data)
             })
         }
