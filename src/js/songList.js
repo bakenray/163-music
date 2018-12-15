4bedfc923d679ed22b9cd2ba7db44e29da66dd3c
bakenray <lpqstrong@163.com>
2018-12-15 17:04:53 +0800

    admin select song

--- src/js/songList.js
+++ src/js/songList.js
@@ -7,12 +7,17 @@
             $el.html(this.template)
             
             let {songs} = data
-            let liList =songs.map((song)=>$('<li></li>').text(song.name))
+            let liList =songs.map((song)=>$('<li></li>').text(song.name).attr('data-song-id',song.id))
             $el.find('ul').empty()
             liList.map((domLi)=>{
                 $el.find('ul').append(domLi)
-
             })
+        },
+        actvieItem(li){
+            let $li =$(li)
+            $li.addClass('active')
+               .siblings('.active')
+               .removeClass('active')
         },
         clearActive(){
             $(this.el).find('.active').removeClass('active')
@@ -21,6 +26,15 @@
     let model={
         data:{
             songs:[]
+        },
+        find(){
+            var query =  new AV.Query('Song')
+            return query.find().then((songs)=>{
+                this.data.songs = songs.map((song)=>{
+                    return {id:song.id,...song.attributes}
+                })
+                return songs
+            })
         }
     }
     let controller={
@@ -28,6 +42,24 @@
             this.view =view
             this.model =model
             this.view.render(this.model.data)
+            this.bindEventHub()
+            this.getAllSongs()
+            this.bindEvents()
+        },
+        getAllSongs(){
+            this.model.find().then(()=>{
+
+                this.view.render(this.model.data)
+            })
+        },
+        bindEvents(){
+            $(this.view.el).on('click','li',(e)=>{
+                this.view.actvieItem(e.currentTarget)
+                let songId =e.currentTarget.getAttribute('data-song-id')
+                window.eventHub.emit('select',{id:songId})
+            })
+        },
+        bindEventHub(){
             window.eventHub.on('upload',()=>{
                 this.view.clearActive()
             })
